7ba197bcbcecdb8f5bb65669fad7a27b8a4cd709
lyichao <931667248@qq.com>
2021-12-20 22:50:13 +0800

    添加商品页面切图

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -5,12 +5,101 @@
       <el-breadcrumb-item>商品管理</el-breadcrumb-item>
       <el-breadcrumb-item>添加商品</el-breadcrumb-item>
     </el-breadcrumb>
-    <el-card></el-card>
+    <el-card>
+      <el-alert title="添加商品信息" type="info" show-icon :closable="false" center> </el-alert>
+      <el-steps :active="parseInt(activeIndex)" finish-status="success" align-center :space="200">
+        <el-step title="基本信息"></el-step>
+        <el-step title="商品参数"></el-step>
+        <el-step title="商品属性"></el-step>
+        <el-step title="商品图片"></el-step>
+        <el-step title="商品内容"></el-step>
+        <el-step title="完成"></el-step>
+      </el-steps>
+      <el-form :model="addRuleForm" :rules="addRules" ref="addRuleForm" label-position="top">
+        <el-tabs v-model="activeIndex" tab-position="left" :before-leave="beforeTabLeave">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addRuleForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model.number="addRuleForm.goods_price"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model.number="addRuleForm.goods_weight"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model.number="addRuleForm.goods_number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类">
+              <el-cascader v-model="addRuleForm.goods_cat" :options="cateList" :props="cascaderProps" @change="handleChange"></el-cascader>
+            </el-form-item>
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      activeIndex: '0',
+      addRuleForm: {
+        goods_name: '',
+        goods_price: 0,
+        goods_weight: 0,
+        goods_number: 0,
+        goods_cat: [],
+      },
+      addRules: {
+        goods_name: [{ required: true, message: '请输入商品名称', trigger: 'blur' }],
+        goods_price: [{ required: true, message: '请输入商品价格', trigger: 'blur' }],
+        goods_weight: [{ required: true, message: '请输入商品重量', trigger: 'blur' }],
+        goods_number: [{ required: true, message: '请输入商品数量', trigger: 'blur' }],
+      },
+      cateList: [],
+      cascaderProps: {
+        label: 'cat_name',
+        value: 'cat_id',
+        children: 'children',
+        expandTrigger: 'hover',
+      },
+    };
+  },
+  mounted() {
+    this.getCateList();
+  },
+  methods: {
+    //获取商品分类
+    async getCateList() {
+      const { data: res } = await this.$http.get('categories');
+      console.log('获取商品分类:', res);
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg);
+      }
+      this.cateList = res.data;
+    },
+    //监听级联选择器的回调事件
+    handleChange() {
+      if (this.addRuleForm.goods_cat.length !== 3) {
+        this.addRuleForm.goods_cat = [];
+      }
+    },
+    //监听标签页切换之前
+    beforeTabLeave(activeName, oldActivityName) {
+      console.log('activeName,oldActivityName:', activeName, oldActivityName);
+      if (this.addRuleForm.goods_cat.length !== 3) {
+        this.$message.error('请先选择商品分类')
+        return false
+      }
+    },
+  },
+};
 </script>
 
 <style>
