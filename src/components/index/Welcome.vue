f9dcaaff796815b47b54c57a609f9f6a94d4da1d
lyichao <931667248@qq.com>
2021-12-26 22:47:16 +0800

    添加欢迎页面

--- src/components/index/Welcome.vue
+++ src/components/index/Welcome.vue
@@ -1,13 +1,15 @@
 <template>
-    <div>welcome</div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>欢迎页</el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style lang="less" scoped>
-    
 </style>
